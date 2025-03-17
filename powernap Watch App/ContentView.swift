4242a4f86de6d94069cadada7541772c662e25d6
你的名字 <>
2025-03-17 23:43:13 +0800

    更新 ContentView：更改圖標和標題文字

--- powernap Watch App/ContentView.swift
+++ powernap Watch App/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "moon.zzz")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Power Nap App")
         }
         .padding()
     }
