5da48f86e7880af29a44bd78f0204cd935a3a4dd
LEESZUHAN <a82052404@gmail.com>
2025-03-17 22:42:22 +0800

    Initial Commit

---
+++ powernap Watch App/powernapApp.swift
@@ -0,0 +1,17 @@
+//
+//  powernapApp.swift
+//  powernap Watch App
+//
+//  Created by michaellee on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct powernap_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
