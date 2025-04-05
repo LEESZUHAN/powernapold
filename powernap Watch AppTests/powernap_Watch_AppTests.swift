5c99709c6c21b5d3c6e91169e32e1a7c023b9a6b
LEESZUHAN <amichael0524@gmail.com>
2025-04-05 16:51:16 +0800

    Initial commit

---
+++ powernap Watch AppTests/powernap_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  powernap_Watch_AppTests.swift
+//  powernap Watch AppTests
+//
+//  Created by michaellee on 3/17/25.
+//
+
+import Testing
+@testable import powernap_Watch_App
+
+struct powernap_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
